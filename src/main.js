1a0ec728276dea46242bf76303b898fadfbb7d04
realeve@qq.com <realeve@qq.com>
2018-04-24 10:09:41 +0800

    上报错误

--- src/main.js
+++ src/main.js
@@ -31,6 +31,33 @@ FastClick.attach(document.body);
 
 Vue.config.productionTip = false;
 
+import util from "./js/common";
+
+Vue.config.errorHandler = function(e, vm, info) {
+  // handle error
+  // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
+  // 只在 2.2.0+ 可用
+  const state = vm.$store.state;
+
+  let err = util.handleErr(e);
+  err = Object.assign(err, {
+    hook: info,
+    remark: JSON.stringify(state)
+  });
+  console.log(err);
+
+  if (err.err_url.indexOf("localhost") > -1) {
+    return;
+  }
+  const params = Object.assign({}, err, {
+    network_type: state.network_type,
+    s: "/addon/Api/Api/rec_error"
+  });
+  vm.$http.jsonp(state.cdnUrl, {
+    params
+  });
+};
+
 /* eslint-disable no-new */
 new Vue({
   router,
